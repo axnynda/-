a57cd35c7586f038204f5ea5a57045a441d3c074
Anna <Anna@ANNAPC>
2022-06-07 10:32:08 +0300

    Добавьте файлы проекта.

---
+++ Список дел/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using Список_дел.Models;
+using Список_дел.Services;
+
+namespace Список_дел
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<Todomodel> _todoDataList;
+        private FileOServices _fileIOService;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileOServices(PATH);
+            try
+            {
+                _todoDataList = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+
+
+            dgTodolist.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+           
+
+        }
+
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            
+        }
+    }
+ 
+           
+    
+}
---
+++ Список дел/Services/FileOServices.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Список_дел.Models;
+
+namespace Список_дел.Services
+{
+    internal class FileOServices
+    {
+        private readonly string PATH;
+        public FileOServices (string path)
+        {
+            PATH = path;
+        }
+        public BindingList<Todomodel> LoadData()
+        {
+            var FileExists = File.Exists(PATH);
+            if (!FileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<Todomodel>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var FileText=reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<Todomodel>>(FileText);
+            }
+        }
+        public void SaveData(object todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output= JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
